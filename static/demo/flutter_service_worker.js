e78620e126c465d82ffa85c95a344cad83f4dee4
Batmend Ganbaatar <batmend@gege.mn>
2025-02-16 19:20:34 +0800

    update docusaurus

--- static/demo/flutter_service_worker.js
+++ static/demo/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f9c4f8d3910431549d6c9c5933030013
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ab53b1b0cce12acc11238021d37ef17c",
+"flutter_bootstrap.js": "513ffbd5024cfb1c97463810c7f67cc2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
