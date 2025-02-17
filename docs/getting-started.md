72acd669827683235b2caa5611ab7ac1507d5b50
Batmend Ganbaatar <batmend@gege.mn>
2025-02-17 12:54:05 +0800

    update to 4.0.1

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -38,7 +38,7 @@ or, manually add in `pubspec.yaml`
 ```yaml title="pubspec.yaml"
 ...
 dependencies:
-    moment_dart: 4.0.0
+    moment_dart: 4.0.1
 ...
 ```
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "4.0.0",
+              label: "4.0.1",
             },
           },
         },
