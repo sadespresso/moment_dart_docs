347deb6ad31bbb8bc1cad092e4f16f71ae2e327c
Batmend Ganbaatar <batmend@gege.mn>
2025-04-30 18:24:17 +0800

    Deploy website - based on a99f5d0aeeb62fab3d24e07c5e89bc27b4755406

--- demo/flutter_bootstrap.js
+++ demo/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3948183895"
+    serviceWorkerVersion: "3365449079"
   }
 });
